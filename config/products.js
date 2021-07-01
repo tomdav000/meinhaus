62a8311ec1f728988890431e3a1340dd9e53d4d1
Mac Book <macbook@Macs-MacBook-Pro.local>
2021-06-30 23:04:09 -0500

    update products

--- config/products.js
+++ config/products.js
@@ -1,33 +1,25 @@
 const products = [
-	{ "id":"1",
-	  "name":"Miura",
-	  "description":"Miura: 8 Week Periodization Training Programme is guaranteed to build strength and lean out your physique. I created this programme shortly upon healing from a broken humerus, with nerve damage. I needed to rebuild my strength, muscular endurance, and size that resulted from the atrophy that occured, and this programme was the programme that not only gotten me to my previous strength levels, but allowed me to surpass them. Don’t be afraid to push yourself a little further when it comes to reps, at the time of my writing of this program, these were the amount of reps I was capable of. You are capable of much more than you know, and think.",
-	  "cost":"25.00",
-	  "image":"/muira.jpg",
-	  "title":"INSTANT DOWNLOAD",
-	  "info":"Your files will be available to download once payment is confirmed. I don't accept returns, exchanges, or cancellations. But please contact me if you have any problems with your order.",
-	  "quantity":"2",
-	  "link":"https://www.google.com"
-	},
-	{ "id":"2",
-	  "name":"Presslufthammer",
+	{ "id":"0",
+	  "name":"The Presslufthammer",
 	  "description":"The Presslufthammer(Jackhammer) Programme is a highly advance, and challenging program designed specifically for individuals looking to break plateaus while gaining strength, and putting on lean hard muscle. This program is not for beginners and will challenge you to adopt different techniques to your weight training method.",
-	  "cost":"25.00",
+	  "cost":"12.00",
 	  "image":"/presslufthammer.jpg",
 	  "title":"INSTANT DOWNLOAD",
 	  "info":"Your files will be available to download once payment is confirmed. I don't accept returns, exchanges, or cancellations. But please contact me if you have any problems with your order.",
 	  "quantity":"2",
-	  "link":"https://www.google.com"
+	  "link":"https://gumroad.com/l/clexg",
+	  "action":"Enter The Presslufthammer"
 	},
-	{ "id":"3",
-	  "name":"Hammer Series",
-	  "description":"The Smith: Hammer Series is a highly instensive 65 day workout routine that caters to the true nature of Mein Haus and its philosophy of progressing in heavy weights, but with volume.",
-	  "cost":"25.00",
+	{ "id":"1",
+	  "name":"The Smith: Hammer Series, Volume One",
+	  "description":"The Smith: Hammer Series is a highly intensive 65 day workout routine that caters to the true nature of the Mein Haus supporter, pushing yourself constantly, knowing your body and when to rest, and wanting nuance to your programming. The Smith is a bit unconventional in the way it is listed, but its results are proven.",
+	  "cost":"10.00",
 	  "image":"/hammer.jpg",
 	  "title":"INSTANT DOWNLOAD",
-	  "info":"Your files will be available to download once payment is confirmed. I don't accept returns, exchanges, or cancellations. But please contact me if you have any problems with your order.",
+	  "info":"Your files will be available to download once payment is confirmed. Please contact me if you have any problems with your order.",
 	  "quantity":"2",
-	  "link":"https://www.google.com"
+	  "link":"https://gumroad.com/l/mDvQm",
+	  "action":"Buy The Smith"
 	}
 ]
 
