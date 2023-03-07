857f71b6114cef85615a82f59cf59f5e07df4b71
Thomas J. Davis <thomas.junior.davis@gmail.com>
2023-03-07 16:26:03 -0600

    Update routes.js
    Thunderground updates

--- routes/routes.js
+++ routes/routes.js
@@ -1,199 +1,83 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const Article = require('../models/Article');
-const {auth} = require('../config/auth');
-const moment = require('moment');
-const Publishable_Key = 'pk_test_GNoY4oxkGQRiNy4eqDC3iKnv00fXwaheB6';
-const Secret_Key = 'sk_test_D6CWYXkw0cojSNcAgPE09qZz00nvL6l71V';
-const stripe = require('stripe')(Secret_Key);
-const products = require('../config/products');
+const DB = require('../config/db');
 const router = express.Router();
 
-router.get('/greet',(req,res)=>{
-	res.send('Hello welcome to Mein Haus basic route')
-})
+router.get('/test',(req,res)=>{
+	res.send('Hola,Hello,Yo,Konnichiwa,NiHao')
+});
 
-router.get('/',async(req,res)=>{
-	try{
-		const perPage = 15;
-		const page = req.params.page || 1;
-
-		if(req.query.search){
-			const regex = new RegExp(escapeRegex(req.query.search),'gi');
-			await Article.find({title: regex}).sort({createdAt: -1}).skip((perPage * page) - perPage)
-			.limit(perPage).exec(function(err, articles){
-				Article.count().count().exec(function(err,count){
-					if(err) return next(err)
-						res.render('home',{
-							articles: articles,
-							current: page,
-							pages: Math.ceil(count / perPage)
-						})
-				})
-			})
-		} else{
-			await Article.find({}).sort({createdAt: -1}).skip((perPage * page) - perPage)
-			.limit(perPage).exec(function(err, articles){
-				Article.count().count().exec(function(err,count){
-					if(err) return next(err)
-						res.render('home',{
-							articles: articles,
-							current: page,
-							pages: Math.ceil(count / perPage)
-						})
-				})
-			})
-		}
-	} catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
-})
-
-router.get('/:page',async(req,res)=>{
-	try{
-		const perPage = 15;
-		const page = req.params.page || 1;
-		
-		await Article.find({}).sort({createdAt: -1}).skip((perPage * page) - perPage)
-		.limit(perPage).exec(function(err, articles){
-			Article.count().count().exec(function(err,count){
-				if(err) return next(err)
-					res.render('home',{
-						articles: articles,
-						current: page,
-						pages: Math.ceil(count / perPage)
-					})
-			})
-		})
-	} catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
-})
-
-router.get('/api/store',(req,res)=>{
-	res.render('store',{products:products});
-})
-
-router.get('/api/store/:id',(req,res)=>{
-	const product = products.find(product => product.id === req.params.id);
-	res.render('product',{key: Publishable_Key,product: product})
-})
-
-router.post('/payment', function(req, res){
-    // Moreover you can take more details from user
-    // like Address, Name, etc from form
-    stripe.customers.create({
-        email: req.body.stripeEmail,
-        source: req.body.stripeToken
-    })
-    .then((customer) => {
-  
-        return stripe.charges.create({
-            amount: 2500,    
-            description: req.body.description,
-            currency: 'usd',
-            customer: customer.id
-        });
-    })
-    .then((charge) => {
-        res.render('success')  // If no error occurs
-    })
-    .catch((err) => {
-        res.send(err)       // If some error occurs
-    });
-})
-  
-
-
-router.get('/api/success',(req,res)=>{
-	res.render('success');
-})
-
-router.get('/api/about',(req,res)=>{
+router.get('/about',(req,res)=>{
 	res.render('about');
-})
+});
 
-router.get('/api/contact',(req,res)=>{
+router.get('/contact',(req,res)=>{
 	res.render('contact');
+});
+
+router.get('/downloads',(req,res)=>{
+	res.render('downloads');
+});
+
+router.get('/meinhaus',(req,res)=>{
+	res.render('meinhaus');
+});
+
+router.get('/davis',(req,res)=>{
+	res.render('davis');
+});
+
+router.get('/thunderground',(req,res)=>{
+	res.render('thunderground');
+});
+
+router.get('/goons',(req,res)=>{
+	res.render('goons')
 })
 
-router.get('/api/downloads',(req,res)=>{
-	res.render('downloads')
+router.get('/',(req,res)=>{
+	res.render('home');
 })
 
-router.get('/articles/:id',async(req,res)=>{
-	const article = await Article.findById(req.params.id).populate('postedBy');
-	res.render('article',{article: article, moment:moment});
+router.get('/home',async(req,res)=>{
+	await DB.query('SELECT * FROM posts',(err,posts)=>{
+		if(err) throw err;
+		res.render('home',{posts:posts});
+	})
 })
 
-
-
-router.get('/articles',async(req,res)=>{
-	try{
-		const articles = await Article.find({}).sort({createdAt: 1})
-		res.status(200).send(articles)
-	}catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
+router.get('/:id',async(req,res)=>{
+	const id = req.params.id;
+	await DB.query('SELECT * FROM posts WHERE id=?',[id],(err,results)=>{
+		if(err) throw err;
+		res.send(results);
+	})
 })
 
-router.get('/articles/:id',async(req,res)=>{
-	try{
-		const article = await Article.findById(req.params.id)
-		res.status(200).send(article)
-	}catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
+router.get('/remove/:id',async(req,res)=>{
+	const id = req.params.id;
+	await DB.query('DELETE FROM posts WHERE id=?',[id],(err,results)=>{
+		if(err) throw err;
+		res.redirect('/api/postages');
+	})
 })
 
-router.put('/articles/:id',async(req,res)=>{
-	try{
-		const article = await Article.findByIdAndUpdate(req.params.id)
-		article.title = req.body.title;
-		article.description = req.body.description;
-		article.training = req.body.training;
-		article.link = req.body.link;
-		await article.save();
-		res.status(200).redirect('/articles')
-	}catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
+router.post('/',async(req,res)=>{
+	const title = req.body.title;
+	const description = req.body.description;
+	await DB.query('INSERT INTO posts SET title=?, description=?',[title,description],(err,results)=>{
+		if(err) throw err;
+		res.redirect('/api/postages')
+	})
 })
 
-router.delete('/articles/:id',async(req,res)=>{
-	try{
-		await Article.findByIdAndRemove(req.params.id)
-		res.status(200).redirect('/articles')
-	}catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
+router.post('/update/:id',async(req,res)=>{
+	const id = req.params.id;
+	const title = req.body.title;
+	const description = req.body.description;
+	await DB.query('UPDATE posts SET title=?, description=? WHERE id=?',[title,description,id],(err,results)=>{
+		if(err) throw err;
+		res.redirect('/api/postages')
+	})
 })
-
-router.post('/articles',async(req,res)=>{
-	try{
-		let newArticle = new Article({
-			title: req.body.title,
-			description: req.body.description,
-			training: req.body.training,
-			link: req.body.link
-		});
-		await newArticle.save();
-		res.status(200).redirect('/articles')
-	}catch(err){
-		console.error(err);
-		res.status(500).send('Not Available...')
-	}
-})
-
-function escapeRegex(text){
-	return text.replace(/[-[\]{}*+?.,\\^$|#\s]/g,"\\$&");
-};
-
 
 module.exports = router;
